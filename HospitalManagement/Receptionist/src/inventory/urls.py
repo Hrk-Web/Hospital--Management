e028cca24fd3f61ce877d297234c768a83411687
Hritik <ankitahritik46@gmail.com>
2020-11-09 22:18:17 +0530

    inventory work is completed

--- HospitalManagement/Receptionist/src/inventory/urls.py
+++ HospitalManagement/Receptionist/src/inventory/urls.py
@@ -7,4 +7,5 @@ inventory = Inventory()
 urlpatterns = [
     path('dashboard', inventory.dashboard, name="show"),
     path('stocks', inventory.stocks, name="stocks"),
+    path('add-medicine', inventory.update_stocks, name="update_stock"),
 ]
